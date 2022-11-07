0ed13c7276a2e544c2df5029529ba96bd81e0373
Elvis Garcia <elvisgarcia93@gmail.com>
2022-11-07 13:35:29 -0500

    Create a Start File

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("Emil and Elvis Collab");
+
+// TO DO 
+  // EMIL RESPOND WITH A CONSOLE LOG OF YOUR OWN BELOW: 
+// Step 1: You'll write a console.log()
+// Step 2: You do the git stuff: add, commit, push  
+   // You did you job
+// Elvis Turn: 
+  // I pull: 
+    // I get your updates, and I should automatically see your console.log();
